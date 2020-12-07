e30b5d4e4bcc74445394e0c51e2bbceb43f454a2
Tyareenbajka <58476139+Tyareenbajka@users.noreply.github.com>
2020-12-07 10:41:14 +0100

    Update modelchecker.pl

--- Labb3/modelchecker.pl
+++ Labb3/modelchecker.pl
@@ -7,6 +7,17 @@
 % swipl
 % ['modelchecker.pl'].
 
+
+
+% check(T, L, S, U, F)
+% T - The transitions in form of adjacency lists
+% L - The labeling
+% S - Current state
+% U - Currently recorded states
+% F - CTL Formula to check.
+%
+
+
 verify(Input) :-
     see(Input), read(T), read(L), read(S), read(F), seen,
     check(T, L, S, [], F).
