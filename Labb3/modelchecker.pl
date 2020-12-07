499ed741044dc10f8a0dff8723745961cabee407
Tyareenbajka <58476139+Tyareenbajka@users.noreply.github.com>
2020-12-07 08:55:53 +0100

    new file

---
+++ Labb3/modelchecker.pl
@@ -0,0 +1,17 @@
+% Adeel Hussain & Philip Tonaczew
+% Labb 3
+%
+%
+%
+% cd C:/Users/tonac/Documents/GitHub/DD1351/Labb3/
+% swipl
+% ['modelchecker.pl'].
+
+verify(Input) :-
+    see(Input), read(T), read(L), read(S), read(F), seen,
+    check(T, L, S, [], F).
+
+% Literals
+check(_, L, S, [], X).
+check(_, L, S, [], neg(X)).
+
