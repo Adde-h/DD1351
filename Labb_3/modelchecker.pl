a4888609b4a97e556712e65ce4eddce12dde12b9
Adeel Hussain <60479388+Adde-h@users.noreply.github.com>
2020-12-08 17:28:43 +0100

    Added few rules

--- Labb_3/modelchecker.pl
+++ Labb_3/modelchecker.pl
@@ -8,7 +8,6 @@
 % ['modelchecker.pl'].
 
 
-
 % check(T, L, S, U, F)
 % T - The transitions in form of adjacency lists
 % L - The labeling
@@ -18,28 +17,66 @@
 %
 
 
+% T = [s2, [s0]]
+% L = [s2, [q]]
+% S = s2.
+
 verify(Input) :-
     see(Input), read(T), read(L), read(S), read(F), seen,
     check(T, L, S, [], F).
 
 % Literals
-check(_, L, S, [], X).
-check(_, L, S, [], neg(X)).
+check(_, L, S, [], X):-
+    member([S,Q], L),
+    member(X,Q),
+    check(_, L, S, [], X),
+    write('Literals!\n').
 
-% And
-check(T, L, S, [], and(F,G)).
+check(_, L, S, [], neg(X)):-
+    member([S,Q], L),
+    \+ member(X,Q),
+    write('Neg Literals!\n').
 
+% And - Valid 087
+check(T, L, S, [], and(X1,X2)):-
+    check(T, L, S, [], X1),
+    check(T, L, S, [], X2),
+    write('And!\n').
 
-% Or
+% Or - valid 035
+check(T, L, S, [], or(X1,X2)):-
+    check(T, L, S, [], X1);
+    check(T, L, S, [], X2),
+    write('Or!\n').
 
-% AX - Nästa väg gäller fi
+% AX - Alla nästa väg gäller fi - valid175
+check(T, L, S, [], ax(X)):-
+    member([S,Q],T),
+    checkAX(L,X,Q),
+    write('Ax!\n').
 
-% EX - I något av nästa vägarna gäller fi
+checkAX(_,_,[]).
+checkAX(L,X,[Q|Tail]):-
+    member([Q,L2],L),
+    member(X,L2),
+    checkAX(L,X,Tail).
 
-% AG - För alla vägar gäller fi alltid
+% EX - I något av nästa vägarna gäller fi - valid 005
+check(T, L, S, [], ex(X)):-
+    member([S,Q],T),
+    checkEX(L,X,Q),
+    write('Ex!\n').
 
-% EG - Det finns en väg där alltid fi gäller
+checkEX(_,_,[]).
+checkEX(L,X,[Q|Tail]):-
+    member([Q,L2],L),
+    member(X,L2);
+    checkEX(L,X,Tail).
+    
+% AG - För alla vägar gäller fi alltid - valid151
 
-% EF - Det finns en väg där så småningom fi gäller
+% EG - Det finns en väg där alltid fi gäller - valid016
 
-% AF - I något av vägarna gäller fi så småningom
+% EF - Det finns en väg där så småningom fi gäller - valid023
+
+% AF - Över alla vägar gäller fi så småningom - valid004
